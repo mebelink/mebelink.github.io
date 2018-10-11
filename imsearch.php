5e969a51dad10b28904a1863a25f41f064a4a2e4
mebelink <mebelink1980@gmail.com>
2018-10-11 18:32:24 +0300

    22

---
+++ imsearch.php
@@ -0,0 +1,165 @@
+<?php require_once("res/x5engine.php"); ?>
+<!DOCTYPE html><!-- HTML5 -->
+<html prefix="og: http://ogp.me/ns#" lang="ru-RU" dir="ltr">
+	<head>
+		<title>Поиск - Студия мебели «Mebelink»</title>
+		<meta charset="utf-8" />
+		<!--[if IE]><meta http-equiv="ImageToolbar" content="False" /><![endif]-->
+		<meta property="og:locale" content="ru" />
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="https://mebelink.ml/imsearch.php" />
+		<meta property="og:title" content="Поиск" />
+		<meta property="og:site_name" content="Студия мебели «Mebelink»" />
+		<meta property="og:image" content="https://mebelink.ml/favImage.png" />
+		<meta property="og:image:type" content="image/png">
+		<meta property="og:image:width" content="744">
+		<meta property="og:image:height" content="450">
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		
+		<link rel="stylesheet" href="style/reset.css?16-2-1-0" media="screen,print" />
+		<link rel="stylesheet" href="style/print.css?16-2-1-0" media="print" />
+		<link rel="stylesheet" href="style/style.css?16-2-1-0" media="screen,print" />
+		<link rel="stylesheet" href="style/template.css?16-2-1-0" media="screen" />
+		<link rel="stylesheet" href="pcss/imsearch.css?16-2-1-0-636748790650214895" media="screen,print" />
+		<script src="res/jquery.js?16-2-1-0"></script>
+		<script src="res/x5engine.js?16-2-1-0" data-files-version="16-2-1-0"></script>
+		<script>
+			window.onload = function(){ checkBrowserCompatibility('Ваш браузер не поддерживает функции, требуемые для визуализации этого Сайта.','Возможно, Ваш браузер не поддерживает функции, требуемые для визуализации этого Сайта.','[1]Обновите Ваш браузер[/1] или [2]продолжите процедуру [/2].','http://outdatedbrowser.com/'); };
+			x5engine.utils.currentPagePath = 'imsearch.php';
+			x5engine.boot.push(function () { x5engine.imPageToTop.initializeButton({}); });
+		</script>
+		<link rel="icon" href="favicon.png?16-2-1-0-636748790650058649" type="image/png" />
+	</head>
+	<body>
+		<div id="imPageExtContainer">
+			<div id="imPageIntContainer">
+				<div id="imHeaderBg"></div>
+				<div id="imFooterBg"></div>
+				<div id="imPage">
+					<header id="imHeader">
+						
+						<div id="imHeaderObjects"><div id="imHeader_imMenuObject_07_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_7" class="" > <div id="imHeader_imCellStyleGraphics_7"></div><div id="imHeader_imCellStyle_7" ><div id="imHeader_imMenuObject_07"><div class="hamburger-button hamburger-component"><div><div><div class="hamburger-bar"></div><div class="hamburger-bar"></div><div class="hamburger-bar"></div></div></div></div><div class="hamburger-menu-background-container hamburger-component">
+	<div class="hamburger-menu-background menu-mobile menu-mobile-animated hidden">
+		<div class="hamburger-menu-close-button"><span>&times;</span></div>
+	</div>
+</div>
+<ul class="menu-mobile-animated hidden">
+	<li class="imMnMnFirst imPage" data-link-paths=",/index.html,/">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="index.html">
+Главная		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths=",/---------1.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="---------1.html">
+Страница 1		</a>
+</div>
+</div>
+	</li><li class="imMnMnLast imLevel"><div class="label-wrapper"><div class="label-inner-wrapper"><span class="label">Уровень 1</span></div></div><ul data-original-position="bottom" class="bottom">
+	<li class="imMnMnFirst imPage" data-link-paths=",/---------2.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="---------2.html">
+Страница 2		</a>
+</div>
+</div>
+	</li><li class="imMnMnLast imPage" data-link-paths=",/---------3.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="---------3.html">
+Страница 3		</a>
+</div>
+</div>
+	</li></ul></li></ul></div><script>
+x5engine.boot.push(function(){x5engine.initMenu('imHeader_imMenuObject_07',1000)});
+$(function () {$('#imHeader_imMenuObject_07 ul li').not('.imMnMnSeparator').each(function () {    var $this = $(this), timeout = 0;    $this.on('mouseenter', function () {        if($(this).parents('#imHeader_imMenuObject_07-menu-opened').length > 0) return;         clearTimeout(timeout);        setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeIn(); }, 250);    }).on('mouseleave', function () {        if($(this).parents('#imHeader_imMenuObject_07-menu-opened').length > 0) return;         timeout = setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeOut(); }, 250);    });});});
+
+</script>
+</div></div></div><div id="imHeader_imObjectTitle_10_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_10" class="" > <div id="imHeader_imCellStyleGraphics_10"></div><div id="imHeader_imCellStyle_10" ><div id="imHeader_imObjectTitle_10"><span id ="imHeader_imObjectTitle_10_text" >(097) 527-02-00 Киевстар (095) 106-00-00  Vodafone</span > </div></div></div></div><div id="imHeader_imObjectTitle_11_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_11" class="" > <div id="imHeader_imCellStyleGraphics_11"></div><div id="imHeader_imCellStyle_11" ><div id="imHeader_imObjectTitle_11"><span id ="imHeader_imObjectTitle_11_text" >(097) 527-02-00 Киевстар (095) 106-00-00  Vodafone</span > </div></div></div></div></div>
+					</header>
+					<div id="imStickyBarContainer">
+						<div id="imStickyBarGraphics"></div>
+						<div id="imStickyBar">
+							<div id="imStickyBarObjects"><div id="imStickyBar_imMenuObject_01_wrapper" class="template-object-wrapper"><div id="imStickyBar_imCell_1" class="" > <div id="imStickyBar_imCellStyleGraphics_1"></div><div id="imStickyBar_imCellStyle_1" ><div id="imStickyBar_imMenuObject_01"><div class="hamburger-button hamburger-component"><div><div><div class="hamburger-bar"></div><div class="hamburger-bar"></div><div class="hamburger-bar"></div></div></div></div><div class="hamburger-menu-background-container hamburger-component">
+	<div class="hamburger-menu-background menu-mobile menu-mobile-animated hidden">
+		<div class="hamburger-menu-close-button"><span>&times;</span></div>
+	</div>
+</div>
+<ul class="menu-mobile-animated hidden">
+	<li class="imMnMnFirst imPage" data-link-paths=",/index.html,/">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="index.html">
+Главная		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths=",/---------1.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="---------1.html">
+Страница 1		</a>
+</div>
+</div>
+	</li><li class="imMnMnLast imLevel"><div class="label-wrapper"><div class="label-inner-wrapper"><span class="label">Уровень 1</span></div></div><ul data-original-position="bottom" class="bottom">
+	<li class="imMnMnFirst imPage" data-link-paths=",/---------2.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="---------2.html">
+Страница 2		</a>
+</div>
+</div>
+	</li><li class="imMnMnLast imPage" data-link-paths=",/---------3.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="---------3.html">
+Страница 3		</a>
+</div>
+</div>
+	</li></ul></li></ul></div><script>
+x5engine.boot.push(function(){x5engine.initMenu('imStickyBar_imMenuObject_01',1000)});
+$(function () {$('#imStickyBar_imMenuObject_01 ul li').not('.imMnMnSeparator').each(function () {    var $this = $(this), timeout = 0;    $this.on('mouseenter', function () {        if($(this).parents('#imStickyBar_imMenuObject_01-menu-opened').length > 0) return;         clearTimeout(timeout);        setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeIn(); }, 250);    }).on('mouseleave', function () {        if($(this).parents('#imStickyBar_imMenuObject_01-menu-opened').length > 0) return;         timeout = setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeOut(); }, 250);    });});});
+
+</script>
+</div></div></div></div>
+						</div>
+					</div>
+					<a class="imHidden" href="#imGoToCont" title="Заголовок главного меню">Перейти к контенту</a>
+					<div id="imSideBarContainer">
+						<div id="imSideBarGraphics"></div>
+						<div id="imSideBar">
+							<div id="imSideBarObjects"></div>
+						</div>
+					</div>
+					<div id="imContentContainer">
+						<div id="imContentGraphics"></div>
+						<main id="imContent">
+							<a id="imGoToCont"></a>
+				<div id="imSearchPage">
+				<h1 id="imPgTitle">Результаты поиска</h1>
+				<?php
+				$search = new imSearch();
+				$keys = isset($_GET['search']) ? @htmlspecialchars($_GET['search']) : "";
+				$page = isset($_GET['page']) ? @htmlspecialchars($_GET['page']) : 0;
+				$type = isset($_GET['type']) ? @htmlspecialchars($_GET['type']) : "pages"; ?>
+				<div class="searchPageContainer">
+				<?php echo $search->search($keys, $page, $type); ?>
+				</div>
+				</div>
+				
+							<div class="imClear"></div>
+						</main>
+					</div>
+					<footer id="imFooter">
+						<div id="imFooterObjects"><div id="imFooter_imObjectTitle_18_wrapper" class="template-object-wrapper"><div id="imFooter_imCell_18" class="" > <div id="imFooter_imCellStyleGraphics_18"></div><div id="imFooter_imCellStyle_18" ><div id="imFooter_imObjectTitle_18"><span id ="imFooter_imObjectTitle_18_text" >"Mebelink"</span > </div></div></div></div></div>
+					</footer>
+				</div>
+				<span class="imHidden"><a href="#imGoToCont" title="Прочесть эту страницу заново">Назад к содержимому</a></span>
+			</div>
+		</div>
+		
+		<noscript class="imNoScript"><div class="alert alert-red">Для использования этого сайта необходимо включить JavaScript.</div></noscript>
+	</body>
+</html>
---
+++ res/access.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+// Users data
+$imSettings['access']['webregistrations_gid'] = '12ldqupz';
+$imSettings['access']['users'] = array(
+	'TIOL@tiol.com' => array(
+		'groups' => array('c2jw1f9y'),
+		'id' => 'iwq0djf6',
+		'firstname' => 'Admin',
+		'lastname' => '',
+		'password' => 'MF2E02c5xhPb',
+		'email' => 'TIOL@tiol.com',
+		'page' => false
+	)
+);
+
+// Admins list
+$imSettings['access']['admins'] = array('iwq0djf6');
+
+// Page/Users permissions
+$imSettings['access']['pages'] = array();
+
+// End of file access.inc.php
