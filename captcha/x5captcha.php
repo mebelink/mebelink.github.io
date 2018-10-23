ec5e01c23e69e6fa663a2ff6c7c5b24bf071f45e
mebelink <mebelink1980@gmail.com>
2018-10-23 11:54:57 +0300

    8

--- captcha/x5captcha.php
+++ captcha/x5captcha.php
@@ -1,7 +1,7 @@
 <?php
 include("../res/x5engine.php");
-$nameList = array("u83","fjc","hs5","xye","lna","jax","hjr","yap","46k","5s5");
-$charList = array("T","N","A","A","4","J","5","D","8","4");
+$nameList = array("he7","mca","lzr","pc6","axa","f5p","w6c","ruf","a4c","rsl");
+$charList = array("6","2","Y","N","N","E","V","2","X","N");
 $cpt = new X5Captcha($nameList, $charList);
 //Check Captcha
 if ($_GET["action"] == "check")
