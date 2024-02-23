2992845a14b2057521b2dbafedc6b6031d4feb80
vannalux <31038037+vannalux@users.noreply.github.com>
2024-02-23 23:00:25 +0200

    55

--- imsearch.php
+++ imsearch.php
@@ -21,7 +21,7 @@
 		<link rel="stylesheet" href="style/print.css?2023-1-5-0" media="print" />
 		<link rel="stylesheet" href="style/style.css?2023-1-5-0" media="screen,print" />
 		<link rel="stylesheet" href="style/template.css?2023-1-5-0" media="screen" />
-		<link rel="stylesheet" href="pcss/imsearch.css?2023-1-5-0-638443258238086169" media="screen,print" />
+		<link rel="stylesheet" href="pcss/imsearch.css?2023-1-5-0-638443259658331711" media="screen,print" />
 		<script src="res/jquery.js?2023-1-5-0"></script>
 		<script src="res/x5engine.js?2023-1-5-0" data-files-version="2023-1-5-0"></script>
 		<script>
@@ -29,7 +29,7 @@
 			x5engine.utils.currentPagePath = 'imsearch.php';
 			x5engine.boot.push(function () { x5engine.imPageToTop.initializeButton({}); });
 		</script>
-		<link rel="icon" href="favicon.ico?2023-1-5-0-638443258237806340" type="image/vnd.microsoft.icon" />
+		<link rel="icon" href="favicon.ico?2023-1-5-0-638443259658072606" type="image/vnd.microsoft.icon" />
 		<style>
 		h1, h2, h3, h4, h5, h6 {
 		    font-weight: normal;
