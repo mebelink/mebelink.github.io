4d9333dcf1824525169d260d3ae358ec8abda662
vannalux <31038037+vannalux@users.noreply.github.com>
2024-03-18 11:32:54 +0200

    55

--- captcha/x5captcha.php
+++ captcha/x5captcha.php
@@ -1,7 +1,7 @@
 <?php
 include("../res/x5engine.php");
-$nameList = array("ruy","76c","rmy","e5c","prp","v3p","v24","xcv","3l4","j7h");
-$charList = array("D","L","8","R","C","X","D","7","E","D");
+$nameList = array("xlu","hwf","32n","5dv","sv2","lfp","68a","xal","4lz","6en");
+$charList = array("8","G","A","3","4","V","K","V","A","2");
 $cpt = new X5Captcha($nameList, $charList);
 //Check Captcha
 if ($_GET["action"] == "check")
